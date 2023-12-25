af63a2a8349afeb4d6d9f929ae8815b264708f84
hongdiem_roro <mingukmanse673@gmail.com>
2023-12-25 11:04:50 +0700

    fix db

--- src/config/conectDB.js
+++ src/config/conectDB.js
@@ -1,11 +1,27 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-const conectDB = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "sa123",
-  database: "pbl_4",
-});
+let dbConfig;
+
+// Check if the application is running in development mode
+if (process.env.NODE_ENV === "development") {
+  dbConfig = {
+    host: "127.0.0.1",
+    user: "root",
+    password: "sa123",
+    database: "pbl_4",
+  };
+} else {
+  dbConfig = {
+    host: process.env.MYSQL_ADDON_HOST,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB,
+    port: process.env.MYSQL_ADDON_PORT,
+  };
+}
+
+const conectDB = mysql.createConnection(dbConfig);
 
 conectDB.connect((err) => {
   if (err) {
